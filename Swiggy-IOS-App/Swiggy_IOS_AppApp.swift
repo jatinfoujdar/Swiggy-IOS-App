c5049b2d03147197a9f4a10207051a566e745ec5
Jatin Foujdar <jatin321001@gmail.com>
2024-11-28 21:32:07 +0530

    Initial Commit

---
+++ Swiggy-IOS-App/Swiggy_IOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiggy_IOS_AppApp.swift
+//  Swiggy-IOS-App
+//
+//  Created by jatin foujdar on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Swiggy_IOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

a101f14e16d95c4463ad11453b87c96bc9d5c8f8
Jatin Foujdar <jatin321001@gmail.com>
2024-12-06 21:01:41 +0530

    navbar commit

--- Swiggy-IOS-App/View/NavbarView.swift
+++ Swiggy-IOS-App/View/NavbarView.swift
@@ -1,25 +1,40 @@
-//
-//  NavbarView.swift
-//  Swiggy-IOS-App
-//
-//  Created by jatin foujdar on 06/12/24.
-//
-
 import SwiftUI
 
 struct NavbarView: View {
+    let iconNames = [
+        ("arrowshape.backward.circle", "Home"),
+        ("fork.knife.circle", "Food"),
+        ("bookmark", "Eatlist"),
+        ("icons8-rocket-64", "Bolt"),
+        ("creditcard", "Card"),
+        ("cart", "Cart")
+    ]
+    
     var body: some View {
-        VStack{
-            HStack(spacing: 40){
-                Image(systemName: "arrowshape.backward.circle")
-                Image(systemName: "fork.knife.circle")
-                Image(systemName: "bookmark")
-                Image(systemName: "house")
-                Image(systemName: "house")
-                Image(systemName: "house")
+        HStack {
+            ForEach(iconNames, id: \.0) { icon in
+                VStack {
+                    // Icon
+                    if icon.0.contains("icons8-rocket-64") {
+                        Image(icon.0)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25, height: 25)
+                    } else {
+                        Image(systemName: icon.0)
+                            .font(.system(size: 25))
+                            .frame(width: 25, height: 25)
+                    }
+                    
+                  
+                    Text(icon.1)
+                        .font(.caption)
+                        .foregroundColor(.black)
+                }
+                .frame(maxWidth: .infinity)
             }
-            .font(.title2)
         }
+        .padding()
     }
 }
 

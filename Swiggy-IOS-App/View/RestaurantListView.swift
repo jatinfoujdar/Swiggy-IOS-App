9728419ae064179a4a138ad29c040a858360f878
Jatin Foujdar <jatin321001@gmail.com>
2024-12-11 01:38:10 +0530

    navbar commit

--- Swiggy-IOS-App/View/RestaurantListView.swift
+++ Swiggy-IOS-App/View/RestaurantListView.swift
@@ -59,6 +59,7 @@ struct RestaurantListView: View {
                     .refreshable {
                         await refreshData()
                     }
+                          NavbarView()
                 }
             }
             .onAppear {

9b3ca29549f6cdb6d46c44a0b9cf3dc7f5b56331
Jatin Foujdar <jatin321001@gmail.com>
2024-12-12 14:11:10 +0530

    iamge slidder commit

--- Swiggy-IOS-App/View/CardSliderView.swift
+++ Swiggy-IOS-App/View/CardSliderView.swift
@@ -9,9 +9,26 @@ struct CardSliderView: View {
             Color.orange
             .ignoresSafeArea()
             VStack{
-                LottieView(animation: .named("fast-shopping-delivery"))
-                    .playbackMode(.playing(.toProgress(1, loopMode: .loop)))
-                    .frame(width: 300, height: 300)
+                Image("logow")
+                HStack{
+                    Text("Swiggy HDFC Bank Credit Card")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                        .multilineTextAlignment(.center)
+                    
+                    Image("cardsss")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                }
+                .padding()
+                
+                HStack{
+                    LottieView(animation: .named("fast-shopping-delivery"))
+                        .playbackMode(.playing(.toProgress(1, loopMode: .loop)))
+                        .frame(width: 300, height: 300)
+               
+                }
             }
         }
     }

939c35297ea2535b522c9d8254351581aaf192ed
Jatin Foujdar <jatin321001@gmail.com>
2024-12-18 18:01:05 +0530

    search bar commit

--- Swiggy-IOS-App/View/Components/Scenes/CarePackage/SearchUIView.swift
+++ Swiggy-IOS-App/View/Components/Scenes/CarePackage/SearchUIView.swift
@@ -1,38 +1,33 @@
-//
-//  SearchUIView.swift
-//  Swiggy-IOS-App
-//
-//  Created by jatin foujdar on 18/12/24.
-//
 
 import SwiftUI
 
 struct SearchUIView: View {
     @State private var searchText: String = ""
-    
+
     var body: some View {
-        HStack{
+        HStack {
+           
             HStack {
                 Image(systemName: "magnifyingglass")
                     .foregroundColor(.gray)
                 TextField("Search for 'Grocery'", text: $searchText)
                     .padding(.vertical, 15)
-                
-                
             }
             .padding(.horizontal)
             .background(Color.white)
             .cornerRadius(25)
             .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 3)
             .padding(.horizontal, 10)
-            
+
             Image(systemName: "cart")
                 .font(.title)
-                .padding(.horizontal,16)
+                .padding(14)
+                .background(
+                    Circle()
+                        .fill(Color.white)
+                )
+                .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 3)
         }
-       
-    
-      
     }
 }
 

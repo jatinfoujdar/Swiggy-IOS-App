4a6a8395543fc0448dcff2b7d9642912379b16af
Jatin Foujdar <jatin321001@gmail.com>
2025-01-05 15:08:21 +0530

    enum apikey commit

--- Swiggy-IOS-App/Utils/ScrollViewWithOffset.swift
+++ Swiggy-IOS-App/Utils/ScrollViewWithOffset.swift
@@ -1,35 +1,18 @@
-import SwiftUI
+import Foundation
 
-
-struct ScrollViewWithOffset<Content: View>: View {
-    let onScroll: (CGPoint) -> Void
-    let content: () -> Content
-
-    init(onScroll: @escaping (CGPoint) -> Void, @ViewBuilder content: @escaping () -> Content) {
-        self.onScroll = onScroll
-        self.content = content
-    }
-
-    var body: some View {
-        ScrollView {
-            GeometryReader { geo in
-                Color.clear
-                    .preference(key: ScrollOffsetKey.self, value: geo.frame(in: .global).origin)
-            }
-            .frame(height: 0) // Used only for offset tracking
-
-            VStack {
-                content()
-            }
+enum ApiKey {
+    static var `default`: String {
+        guard let filePath = Bundle.main.url(forResource: "GenerativeInfo", withExtension: "plist")
+        else {
+            fatalError("Couldn't find file 'GenerativeInfo.plist'")
         }
-        .onPreferenceChange(ScrollOffsetKey.self, perform: onScroll)
-    }
-}
-
-struct ScrollOffsetKey: PreferenceKey {
-    static var defaultValue: CGPoint = .zero
-
-    static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {
-        value = nextValue()
+        guard let plist = NSDictionary(contentsOf: filePath),
+              let value = plist.object(forKey: "API_KEY") as? String else {
+            fatalError("Couldn't find 'API_KEY' in 'GenerativeInfo.plist'")
+        }
+        if value.starts(with: "_") {
+            fatalError("Follow the instructions at google.dev/tutorials/setup to get an API key.")
+        }
+        return value
     }
 }
